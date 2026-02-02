05f6c928ac5136c0dae1f6dcbb622512f0c15f3f
Ammar kamran ali <ammarkamran2006@gmail.com>
2026-02-02 19:46:55 +0500

    Implement Vehicle class with rent calculation
    Added a Vehicle class with methods to calculate rent and display details.

--- LAB 02/POST_LAB_TASK/Question 01.py
+++ LAB 02/POST_LAB_TASK/Question 01.py
@@ -1,1 +1,29 @@
-gdg
+class Vehicle:
+  
+  def __init__(self, vehicle_id, brand, rent_per_day):
+        self.vehicle_id= vehicle_id
+        self.brand = brand
+        self.rent_per_day = rent_per_day
+
+
+  def cal_rent(self, days):
+        rent= self.rent_per_day* days
+        return rent
+
+  def display_details(self, rent):
+       print("Vehicle ID:", self.vehicle_id)
+       print("Brand:", self.brand)
+       print("Rent per day:", self.rent_per_day)
+       print("Rent:", rent)
+
+# Creating objects
+civic= Vehicle(2345, "Honda", 7000)
+prius= Vehicle(7894, "Toyota",6000)
+
+rent1=civic.cal_rent(5)
+civic.display_details(rent1)
+print()
+rent2=prius.cal_rent(3)
+prius.display_details(rent2)
+
+
