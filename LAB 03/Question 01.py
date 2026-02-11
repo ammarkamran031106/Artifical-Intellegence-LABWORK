55913271383652f3396c33d12eeadd23198bac58
Ammar kamran ali <ammarkamran2006@gmail.com>
2026-02-11 22:38:13 +0500

    Update Question 01.py

--- LAB 03/Question 01.py
+++ LAB 03/Question 01.py
@@ -1,1 +1,14 @@
+print("Traffic Light Reflex Agent\n")
+
+conds = ["Heavy Traffic", "Light Traffic"]
+i = 1
+
+for c in conds:
+ print(f"Step {i}")
+ if c == "Heavy Traffic":
+  print("Percept: Heavy Traffic → Action: Extend Green Time")
+ else:
+  print("Percept: Light Traffic → Action: Normal Green")
+ print("-"*40)
+ i += 1
 
